2cbfd6c07e4eb56bbe069a8c1e1497ee75fb5e53
gomeow <davismariotti@gmail.com>
2013-05-11 16:24:11 -0700

    Change variable name

--- src/main/java/com/hawkfalcon/deathswap/DSCommand.java
+++ src/main/java/com/hawkfalcon/deathswap/DSCommand.java
@@ -19,44 +19,44 @@ public class DSCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if(!(sender instanceof Player))
             return false;
-        String n = sender.getName();
+        String name = sender.getName();
         Player player = (Player) sender;
         if((cmd.getName().equalsIgnoreCase("ds")) || (cmd.getName().equalsIgnoreCase("deathswap"))) {
             if(args.length == 0) {
-                plugin.utility.message("Join with /ds join", n);
+                plugin.utility.message("Join with /ds join", name);
             }
         }
         if(args.length == 1) {
             if(args[0].equalsIgnoreCase("join") && player.hasPermission("deathswap.join")) {
-                if(!plugin.game.contains(n) && !plugin.lobby.contains(n)) {
-                    plugin.utility.message("You joined the game!", n);
-                    plugin.utility.broadcastLobby(n + " joined the game!");
+                if(!plugin.game.contains(name) && !plugin.lobby.contains(name)) {
+                    plugin.utility.message("You joined the game!", name);
+                    plugin.utility.broadcastLobby(name + " joined the game!");
                     // mark as in lobby
-                    plugin.lobby.add(n);
+                    plugin.lobby.add(name);
                     // teleport to lobby
-                    plugin.utility.teleport(n, 0);
+                    plugin.utility.teleport(name, 0);
                     plugin.utility.checkForStart();
                 }
             }
             if(args[0].equalsIgnoreCase("leave") && player.hasPermission("deathswap.leave")) {
-                if(plugin.lobby.contains(n)) {
-                    plugin.utility.message("You left the game!", n);
-                    plugin.utility.broadcastLobby(n + " left the game!");
-                    plugin.lobby.remove(n);
-                    plugin.utility.teleport(n, 1);
+                if(plugin.lobby.contains(name)) {
+                    plugin.utility.message("You left the game!", name);
+                    plugin.utility.broadcastLobby(name + " left the game!");
+                    plugin.lobby.remove(name);
+                    plugin.utility.teleport(name, 1);
                 }
-                if(plugin.game.contains(n)) {
-                    plugin.game.remove(n);
-                    plugin.stop.dealWithLeftoverGames(n, false);
+                if(plugin.game.contains(name)) {
+                    plugin.game.remove(name);
+                    plugin.stop.dealWithLeftoverGames(name, false);
                     player.getInventory().clear();
                     plugin.utility.clearArmor(player);
-                    plugin.utility.teleport(n, 1);
+                    plugin.utility.teleport(name, 1);
                 }
             }
             if(args[0].equalsIgnoreCase("accept") && player.hasPermission("deathswap.accept")) {
-                if(plugin.accept.containsKey(n)) {
-                    plugin.start.newGame(plugin.accept.get(n), n);
-                    plugin.accept.remove(n);
+                if(plugin.accept.containsKey(name)) {
+                    plugin.start.newGame(plugin.accept.get(name), name);
+                    plugin.accept.remove(name);
                 }
             }
         }
@@ -73,15 +73,15 @@ public class DSCommand implements CommandExecutor {
                     return false;
                 }
                 plugin.saveConfig();
-                plugin.utility.message(ChatColor.GOLD + "Spawn point set!", n);
+                plugin.utility.message(ChatColor.GOLD + "Spawn point set!", name);
             }
             if(args[0].equalsIgnoreCase("duel") && player.hasPermission("deathswap.duel")) {
                 Player pl = plugin.getServer().getPlayer(args[1]);
                 String o = args[1];
-                if(pl == null || pl.getName() == n || plugin.game.contains(pl.getName())) {
+                if(pl == null || pl.getName() == name || plugin.game.contains(pl.getName())) {
                     sender.sendMessage("Invalid Player");
                 } else {
-                    startAccept(n, o);
+                    startAccept(name, o);
                 }
             }
         }
