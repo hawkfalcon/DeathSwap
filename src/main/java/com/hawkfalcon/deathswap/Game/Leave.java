c15011419aaa9dedb8a6926dcbb6e2a3b2e05c06
gomeow <davismariotti@gmail.com>
2013-07-24 14:50:35 -0700

    Fix leaving

--- src/main/java/com/hawkfalcon/deathswap/Game/Leave.java
+++ src/main/java/com/hawkfalcon/deathswap/Game/Leave.java
@@ -14,15 +14,17 @@ public class Leave {
 
     public void leave(Player player) {
         String name = player.getName();
+        plugin.utility.message("You left the game!", player);
         if (plugin.lobby.contains(name)) {
-            plugin.utility.message("You left the game!", player);
             plugin.utility.broadcastLobby(name + " left the game!");
             plugin.lobby.remove(name);
             plugin.utility.teleport(player, 1);
         }
         if (plugin.game.contains(name)) {
             plugin.utility.restorePlayer(player);
+            plugin.utility.broadcastGame(name + " left the game!");
             plugin.utility.teleport(player, 1);
+            plugin.winGame.winGame(player, false);
         }
     }
 }
